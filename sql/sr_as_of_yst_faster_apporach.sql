12f45fa5c8d93d9eb3b395265782a1aaaf898cb3
Code Miner <41459700+itscodeminer@users.noreply.github.com>
2025-04-11 12:28:21 +0530

    Update sr_as_of_yst_faster_apporach.sql

--- sql/sr_as_of_yst_faster_apporach.sql
+++ sql/sr_as_of_yst_faster_apporach.sql
@@ -28,7 +28,8 @@ BEGIN
     FROM siebel_srs s
     JOIN staff_master sm ON s.siebel_id = sm.emp_id
     WHERE s.closed_date::date BETWEEN date_trunc('month', CURRENT_DATE)::date AND CURRENT_DATE - 1
-    GROUP BY 1,2,3,4,5,6,7,8,9
-    ORDER BY sm.tech_id, closed_day, bucket;
+    GROUP BY sm.tech_id, sm.first_name, sm.state, sm.city, sm.zone, sm.team, s.closed_date::date, 
+             CASE WHEN s.severity > 5 THEN '5+' ELSE s.severity::text END
+    ORDER BY sm.tech_id, s.closed_date::date, bucket;
 END;
 $$ LANGUAGE plpgsql;
