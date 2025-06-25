66b0e905a43cf62d8e8797a24a66e7e0b626e74b
Code Miner <41459700+itscodeminer@users.noreply.github.com>
2025-06-26 01:56:06 +0530

    Update build_json.sql

--- sql/build_json.sql
+++ sql/build_json.sql
@@ -1,16 +1,15 @@
-SELECT json_agg(
-    jsonb_build_object(
-        'field_name', meta.field_name,
-        'field_display_name', meta.field_display_name,
-        'value', row_data.value,
-        'is_track_time', false
-    )
-) AS field_info
-FROM (
-    SELECT *
-    FROM data_table
-    WHERE id = 1  -- or any specific row
-) t,
+SELECT 
+    t.site,
+    json_agg(
+        jsonb_build_object(
+            'field_name', meta.field_name,
+            'field_display_name', meta.field_display_name,
+            'value', row_data.value,
+            'is_track_time', false
+        )
+    ) AS field_info
+FROM data_table t
 LATERAL jsonb_each_text(to_jsonb(t)) AS row_data(key, value)
 JOIN metadata_table meta
-  ON row_data.key = meta.column_name;
+  ON row_data.key = meta.column_name
+GROUP BY t.site, t.id;  -- include the primary key to separate each row
