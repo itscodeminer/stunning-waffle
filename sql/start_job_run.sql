8f773f00480a778191afdc0895525a38c462ea58
Code Miner <41459700+itscodeminer@users.noreply.github.com>
2025-10-09 17:35:27 +0530

    Update start_job_run.sql

--- sql/start_job_run.sql
+++ sql/start_job_run.sql
@@ -1,21 +1,30 @@
 CREATE OR REPLACE FUNCTION start_job_run(
     p_job_name TEXT,
     p_started_at TIMESTAMP,
-    p_next_run_at TIMESTAMP
+    p_interval_value INTEGER,
+    p_interval_unit TEXT
 ) RETURNS INTEGER AS $$
 DECLARE
     v_job_id INTEGER;
     v_log_id INTEGER;
-    v_scheduled_for TIMESTAMP;
+    v_current_next_run TIMESTAMP;
+    v_new_next_run TIMESTAMP;
+    v_interval_expr TEXT;
 BEGIN
-    -- Get job ID and current next_run_at from master
-    SELECT id, next_run_at INTO v_job_id, v_scheduled_for
+    -- Get job ID and current next_run_at
+    SELECT id, next_run_at INTO v_job_id, v_current_next_run
     FROM fts_job_master
     WHERE name = p_job_name;
 
     IF v_job_id IS NULL THEN
         RAISE EXCEPTION 'Job not found: %', p_job_name;
     END IF;
+
+    -- Construct interval expression like '15 minutes'
+    v_interval_expr := p_interval_value || ' ' || p_interval_unit;
+
+    -- Compute new next_run_at
+    v_new_next_run := v_current_next_run + v_interval_expr::interval;
 
     -- Insert into job log
     INSERT INTO fts_job_log (
@@ -26,7 +35,7 @@ BEGIN
     )
     VALUES (
         v_job_id,
-        v_scheduled_for,      -- use existing next_run_at as scheduled_for
+        v_current_next_run,
         'RUNNING',
         p_started_at
     )
@@ -37,7 +46,7 @@ BEGIN
     SET
         last_run_start_at = p_started_at,
         last_run_status = 'RUNNING',
-        next_run_at = p_next_run_at
+        next_run_at = v_new_next_run
     WHERE id = v_job_id;
 
     RETURN v_log_id;
